5581fb9b1dae94b74ec8e0d2c3d00752386173eb
thais <thaisventura@meirelesefreitas.adv.br>
2024-03-11 22:05:30 -0300

    feat: create component Search

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,11 @@
+function  Search(){
+    return (
+        <section>
+            <h2>Ja sabe por onde começar?</h2>
+            <h3>Procure seu livro</h3>
+            <input type="text" placeholder="Digite o nome do livro" />
+        </section>
+    )
+}
+
+export default Search;
